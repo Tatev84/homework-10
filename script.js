0fe5e4a25bdf3ecc5f5305913d151838c57dc895
Tatev84 <tatevh84@gmail.com>
2021-07-26 08:54:39 +0400

    homework 10

---
+++ script.js
@@ -0,0 +1,46 @@
+
+/*  let arr =[2,5,100]
+let str=arr.map(function(v){
+    return v+" "
+
+})
+console.log(str)  */
+
+/* let arr =[2,5,100]
+ let str=arr.map(v=> v+" ")
+ console.log(str)  */
+
+
+ /* let arr=[1,2,8,9]
+ let pow=arr.map(v=> Math.pow(v,2) )
+ console.log(pow) */
+
+ /* let arr=["apple", "mango", "banana", "watermelon", "pie"]
+ let arr1=arr.filter(function(word){
+     if (word.length<=5){
+         return word.length 
+     }
+
+ })
+console.log(arr1) 
+ */
+
+/* 
+let arr=["apple", "mango", "banana", "watermelon", "pie"]
+let arr1=arr.filter(word=> word.length<=5)
+console.log(arr1)  */
+
+/* 
+ let arr=[5,4, 10, 21, 1, 57]
+let num=arr.filter(function(count){
+    if(count>4)
+    return count
+
+
+})
+console.log(num)  */
+
+
+/* let arr=[5,4, 10, 21, 1, 57]
+let num=arr.filter(v=> v>4)
+console.log(num)  */
